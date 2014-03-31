2496bce09d6627bc375d600e1dd2a2ddcd56ab5b
sinh <sinhnx@192.168.1.5>
2014-03-31 21:10:39 +0700

    Session 01: Exception Handling second commit

---
+++ ADF2_Example/src/session01/exception_handling/CheckedExceptionHandling.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package session01.exception_handling;
+
+/**
+ *
+ * @author sinhnx
+ */
+public class CheckedExceptionHandling {
+    
+}
