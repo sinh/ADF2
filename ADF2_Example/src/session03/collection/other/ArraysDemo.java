b9510beaa26eaa75f3175ede9b02d51a2d9bbb62
sinh <sinhnx@192.168.0.81>
2014-04-04 17:38:43 +0700

    Session 03: Arrays & Collections

---
+++ ADF2_Example/src/session03/collection/other/ArraysDemo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session03.collection.other;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sinhnx
+ */
+public class ArraysDemo {
+
+    public static void main(String[] args) {
+        int a[] = {1, 7, 3, 4, 8, 9, 2, 5};
+        //Sử dụng Arrays để sắp xếp:
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+
+        System.out.print("Nhập vào một số nguyên: ");
+        int search = new Scanner(System.in).nextInt();
+        //Sử dụng phương thức tìm kiếm nhị phân
+        int index = Arrays.binarySearch(a, search);
+        System.out.println("Tìm thấy " + search + " ở vị trí thứ " + index);
+    }
+}
---
+++ ADF2_Example/src/session03/collection/other/CollectionsDemo.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session03.collection.other;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import session03.collection.Student;
+
+/**
+ *
+ * @author sinhnx
+ */
+public class CollectionsDemo {
+
+    public static void main(String[] args) {
+        List lst = new ArrayList();
+        //Thêm sinh viên vào danh sách
+        lst.add(new Student("A33333", "Nguyen Van Z"));
+        lst.add(new Student("A11111", "Nguyen Van E"));
+        lst.add(new Student("A55555", "Nguyen Van X"));
+        lst.add(new Student("A22222", "Nguyen Van B"));
+        lst.add(new Student("A44444", "Nguyen Van A"));
+        lst.add(new Student("A66666", "Nguyen Van F"));
+
+        //Sắp xếp theo rollNo vì lớp Student đã implements Comparable interface
+        Collections.sort(lst);
+        System.out.println("Sắp xếp theo mã sinh viên: ");
+        for (int i = 0; i < lst.size(); i++) {
+            System.out.println(lst.get(i));
+        }
+
+        //Thực hiện sắp xếp theo tên sinh viên
+        Collections.sort(lst, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Student && o2 instanceof Student) {
+                    Student s1 = (Student) o1;
+                    Student s2 = (Student) o2;
+                    return s1.getName().compareTo(s2.getName());
+                }
+                return 0;
+            }
+        });
+
+        System.out.println("\nSắp xếp theo tên: ");
+        for (int i = 0; i < lst.size(); i++) {
+            System.out.println(lst.get(i));
+        }
+    }
+}
